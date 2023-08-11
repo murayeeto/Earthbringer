3ea1009b54529ed67e89f3df0250164034886c20
murayeeto <48611042+murayeeto@users.noreply.github.com>
2023-08-10 22:33:18 -0400

    test test test

--- Reader.js
+++ Reader.js
@@ -1,54 +1,53 @@
 (function () {
-    const pdfContainer = document.getElementById('pdfContainer');
-    const prevPageBtn = document.getElementById('prevPage');
-    const nextPageBtn = document.getElementById('nextPage');
+  var pdfContainer = document.getElementById('pdfContainer');
+  var prevPageButton = document.getElementById('prevPage');
+  var nextPageButton = document.getElementById('nextPage');
   
-    let currentPage = 1;
-    let pdfDoc = null;
+  var currentPage = 1;
+  var pdfDoc = null;
   
-    const renderPage = (pageNum) => {
-      pdfDoc.getPage(pageNum).then(page => {
-        const viewport = page.getViewport({ scale: 1.0 });
-        const canvas = document.createElement('canvas');
-        const context = canvas.getContext('2d');
-        canvas.height = viewport.height;
-        canvas.width = viewport.width;
+  var renderPage = function (num) {
+    pdfDoc.getPage(num).then(function (page) {
+      var canvas = document.createElement('canvas');
+      var context = canvas.getContext('2d');
+      var viewport = page.getViewport({ scale: 1.0 });
   
-        const renderContext = {
-          canvasContext: context,
-          viewport: viewport,
-        };
+      // Set canvas dimensions based on container size
+      var containerWidth = pdfContainer.clientWidth - 40; // Adjust for padding
+      var scale = containerWidth / viewport.width;
+      canvas.height = viewport.height * scale;
+      canvas.width = containerWidth;
   
-        pdfContainer.innerHTML = '';
-        pdfContainer.appendChild(canvas);
+      pdfContainer.innerHTML = '';
+      pdfContainer.appendChild(canvas);
   
-        page.render(renderContext);
-      });
-    };
-  
-    const loadPDF = (pdfUrl) => {
-      pdfjsLib.getDocument(pdfUrl).promise.then(pdf => {
-        pdfDoc = pdf;
-        renderPage(currentPage);
-      });
-    };
-  
-    prevPageBtn.addEventListener('click', () => {
-      if (currentPage > 1) {
-        currentPage--;
-        renderPage(currentPage);
-      }
+      page.render({ canvasContext: context, viewport: viewport });
     });
+  };
   
-    nextPageBtn.addEventListener('click', () => {
-      if (currentPage < pdfDoc.numPages) {
-        currentPage++;
-        renderPage(currentPage);
-      }
+  var loadPDF = function (pdfUrl) {
+    pdfjsLib.getDocument(pdfUrl).promise.then(function (pdfDoc_) {
+      pdfDoc = pdfDoc_;
+      renderPage(currentPage);
     });
+  };
   
-    // Load PDF when the page is ready
-    document.addEventListener('DOMContentLoaded', () => {
-      loadPDF('Chapters/Chapter1.pdf');
-    });
+  prevPageButton.addEventListener('click', function () {
+    if (currentPage > 1) {
+      currentPage--;
+      renderPage(currentPage);
+    }
+  });
+  
+  nextPageButton.addEventListener('click', function () {
+    if (currentPage < pdfDoc.numPages) {
+      currentPage++;
+      renderPage(currentPage);
+    }
+  });
+  
+  // Load PDF when the page is ready
+  document.addEventListener('DOMContentLoaded', function () {
+    loadPDF('Chapters/Chapter1.pdf');
+  });
   })();
