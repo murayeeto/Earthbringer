653fc9f8c9af4b120b72f739c939769728826fbe
murayeeto <48611042+murayeeto@users.noreply.github.com>
2023-08-26 02:54:56 -0400

    test

--- Reader.js
+++ Reader.js
@@ -2,52 +2,53 @@
   var pdfContainer = document.getElementById('pdfContainer');
   var prevPageButton = document.getElementById('prevPage');
   var nextPageButton = document.getElementById('nextPage');
-  
+
   var currentPage = 1;
   var pdfDoc = null;
-  
+
   var renderPage = function (num) {
     pdfDoc.getPage(num).then(function (page) {
       var canvas = document.createElement('canvas');
       var context = canvas.getContext('2d');
-      var viewport = page.getViewport({ scale: 1.0 });
-  
-      // Set canvas dimensions based on container size
+
+      // Calculate scale based on container width and page width
       var containerWidth = pdfContainer.clientWidth;
-      var scale = containerWidth / viewport.width;
-      canvas.height = viewport.height * scale;
-      canvas.width = containerWidth;
-  
+      var scale = containerWidth / page.getViewport({ scale: 1 }).width;
+      var viewport = page.getViewport({ scale: scale });
+
+      canvas.height = viewport.height;
+      canvas.width = viewport.width;
+
       pdfContainer.innerHTML = ''; // Clear previous content
       pdfContainer.appendChild(canvas);
-  
+
       page.render({ canvasContext: context, viewport: viewport });
     });
   };
-  
+
   var loadPDF = function (pdfUrl) {
     pdfjsLib.getDocument(pdfUrl).promise.then(function (pdfDoc_) {
       pdfDoc = pdfDoc_;
       renderPage(currentPage);
     });
   };
-  
+
   prevPageButton.addEventListener('click', function () {
     if (currentPage > 1) {
       currentPage--;
       renderPage(currentPage);
     }
   });
-  
+
   nextPageButton.addEventListener('click', function () {
     if (currentPage < pdfDoc.numPages) {
       currentPage++;
       renderPage(currentPage);
     }
   });
-  
+
   // Load PDF when the page is ready
   document.addEventListener('DOMContentLoaded', function () {
     loadPDF('Chapters/Chapter1.pdf');
   });
-  })();
+})();
